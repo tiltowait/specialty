69eec76797ffdeaec0ff17f14b48a63ee272cb23
tiltowait <tiltowaitt+github@icloud.com>
2020-11-26 10:23:11 -0800

    Calculate both specced and unspecced probabilities

--- src/main.rs
+++ src/main.rs
@@ -8,33 +8,49 @@ fn main() {
   let difficulty: i16 = args[2].parse().expect("Difficulty must be a number!");
   let target: i16 = args[3].parse().expect("Target must be a number!");
 
-  let trials = 1_000_000;
+  const TRIALS: i32 = 1_000_000;
 
-  let mut successful_trials = 0;
-  for _ in 0..trials {
-    successful_trials += if roll(pool, difficulty, target) { 1 } else { 0 };
+  // We calculate both the specced (10s are doubled) and unspecced probabilities
+  let mut spec = 0;
+  let mut unspec = 0;
+
+  for _ in 0..TRIALS {
+    let result = roll(pool, difficulty, target);
+
+    spec += if result.0 { 1 } else { 0 };
+    unspec += if result.1 { 1 } else { 0 };
   }
 
-  let ratio = successful_trials as f64 / trials as f64;
-  println!("{} diff {}: {}", pool, difficulty, ratio);
+  let spec_ratio = spec as f64 / TRIALS as f64;
+  let unspec_ratio = unspec as f64 / TRIALS as f64;
+
+  println!("Pool {}, difficulty {}", pool, difficulty);
+  println!("With specialty: {}", spec_ratio);
+  println!("Without:        {}", unspec_ratio);
 }
 
-fn roll(pool: i16, difficulty: i16, target: i16) -> bool {
+/// Returns true/false for spec/unspecced if a roll hit the target successes
+fn roll(pool: i16, difficulty: i16, target: i16) -> (bool, bool) {
   let mut rng = rand::thread_rng();
   let die = Uniform::from(1..=10);
 
-  let mut successes = 0;
+  let mut spec = 0;
+  let mut unspec = 0;
 
   for _ in 1..=pool {
     let throw = die.sample(&mut rng);
 
     if throw == 1 {
-      successes -= 1;
+      unspec -= 1;
     } else if throw == 10 {
-      successes += 2;
+      unspec += 1;
+      spec += 1;
     } else if throw >= difficulty {
-      successes += 1;
+      unspec += 1;
     }
   }
-  successes >= target
+  spec += unspec; // Only difference between spec and unspec are 10s, so we can
+                  // afford to be a little concise here.
+
+  (spec >= target, unspec >= target)
 }
